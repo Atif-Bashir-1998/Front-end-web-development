9bb20d59cbb4766e3f91200fb916bd8b87e85a1f
Atif <adobe8832289@gmail.com>
2020-04-01 18:03:59 +0500

    contact-page done

---
+++ Kitchen-Seller/contact.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover({
+        placement: 'top',
+        trigger: 'hover'
+    });
+
+    AOS.init({
+        delay: 500,
+        duration: 2000
+    });
+
+    
+});
